dc4b6598c73570f218f7d3eac84e1004a04a70d4
ゆんゆん <102103338+IZIDesu@users.noreply.github.com>
2025-05-02 13:36:38 +0100

    Add files via upload

---
+++ config/generalConfig.py
@@ -0,0 +1,53 @@
+
+X_ECRAN = 500
+Y_ECRAN = 500
+
+# Colors
+BLACK =     (  0,   0,   0)
+WHITE =     (255, 255, 255)
+RED =       (255,   0,   0)
+DARK_BLUE = (  0,   0, 255)
+BLUE =      ( 50, 150, 255)
+LIGHT_BLUE =( 50, 250, 255)
+GREEN =     (  0, 255,   0)
+YELLOW =    (255, 255,   0)
+
+CircleColor = WHITE # Lower torso
+L1Color = RED # Upper leg
+L2Color = BLUE # Lower leg
+FootColor = BLACK # Foot
+FootOutlineColor = WHITE # Foot outline
+
+L3Color = YELLOW
+L4Color = GREEN
+
+# Color of first and second leg
+TextColors = [RED, BLUE]
+#    first leg ^    ^ second leg
+
+# frame rate
+FPS = 120
+
+# largura dos segmentos da perna
+l1 = 100
+l2 = 100
+
+l3 = 100
+l4 = 100
+
+# Foot dimensions
+foot_width = 20
+foot_height = 10
+
+# Ground position
+ground_y = 460  # Y-coordinate of the ground
+
+# pos inicial do robo
+robot_x, robot_y = X_ECRAN/2, Y_ECRAN/2 # midle of the screen 
+
+# rectangle with angles
+TextFont = None
+TextSize = 36
+
+TextScreenX = 10
+TextScreenY = 10
